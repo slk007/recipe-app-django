7097ec62b3b35d6cc262b3045caae05eb458da16
Shubham Soni <shubham.soni.lko@gmail.com>
2024-09-05 01:22:47 +0530

    update UserAdmin with fields

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
@@ -8,6 +9,21 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login',]
 
 
 admin.site.register(User, UserAdmin)
