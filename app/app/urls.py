79e36a7eaa52aba694e2ebf33587cb456486a04c
Shubham Soni <shubham.soni.lko@gmail.com>
2024-09-05 10:33:37 +0530

    add create user serializer, view, url

--- app/app/urls.py
+++ app/app/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path(
         'api/docs',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
-         name='api-docs'
+        name='api-docs'
     ),
     path('api/user/', include('user.urls')),
 ]
