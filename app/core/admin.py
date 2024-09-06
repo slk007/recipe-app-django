312aa5d5555ce83be42991c6f0e3470b26cdac61
Shubham Soni <shubham.soni.lko@gmail.com>
2024-09-07 00:28:30 +0530

    add tags model and testcases

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from .models import User, Recipe
+from .models import User, Recipe, Tag
 
 
 class UserAdmin(BaseUserAdmin):
@@ -42,3 +42,4 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Recipe)
+admin.site.register(Tag)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,7 +6,12 @@ from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from ..models import Recipe
+from ..models import Recipe, Tag
+
+
+def create_user(email="user@example.com", password="testpass123"):
+    """Create and return a new user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -64,3 +69,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
