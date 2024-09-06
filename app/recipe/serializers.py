e99b8991f117e10a49b2210df8da7b1dfe9733aa
Shubham Soni <shubham.soni.lko@gmail.com>
2024-09-06 10:18:43 +0530

    add Recipe API Viewset

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,2 +1,11 @@
-class RecipeSerializer:
-    pass
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for Recipes"""
+    class Meta:
+        model = Recipe
+        fields = ["id", "title", "time_minutes", "price", "link"]
+        read_only_fields = ["id"]
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -9,7 +9,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from app.core.models import Recipe
+from core.models import Recipe
 from ..serializers import RecipeSerializer
 
 
