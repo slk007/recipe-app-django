aada959892555cdb0d41270163400486304480f6
Shubham Soni <shubham.soni.lko@gmail.com>
2024-09-11 00:18:17 +0530

    add implementation for listing Ingredient APIs

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import RecipeViewSet, TagViewSet
+from .views import RecipeViewSet, TagViewSet, IngredientViewSet
 
 router = DefaultRouter()
 router.register("recipes", RecipeViewSet)
 router.register("tags", TagViewSet)
+router.register("ingredients", IngredientViewSet)
 
 app_name = "recipe"
 
